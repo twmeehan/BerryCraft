a17e2f29b6a3917651ad123acc0ebf5fb9b1bb3f
Timothy Meehan <tmeehan26@gmail.com>
2024-04-15 16:50:37 -0400

    attempting to add health-display

---
+++ health-display/src/main/java/me/berrycraft/healthdisplay/HealthDisplay.java
@@ -0,0 +1,17 @@
+package me.berrycraft.healthdisplay;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HealthDisplay extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
