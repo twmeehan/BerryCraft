70b76938de55bb6d22d31908ab2896cadf1bc3f8
Timothy <tmeehan26@gmail.com>
2024-06-16 20:02:20 -0400

    Added Coins
    and looting system

--- berry-economy/src/main/java/me/berrycraft/berryeconomy/items/Pinkberry.java
+++ berry-economy/src/main/java/me/berrycraft/berryeconomy/items/Pinkberry.java
@@ -19,35 +19,14 @@ public class Pinkberry extends CustomItem{
     // use new Pinkberry() to get an item stack containing 1 pinkberry
     public Pinkberry() {
 
-        super(Material.PLAYER_HEAD);
+        super(Material.IRON_INGOT);
 
-        setSkull("http://textures.minecraft.net/texture/778308fe4dda1bb8a96f5b226b32542f49fd65bc55b44bbc31343eb400cf5e2");
         ItemMeta meta = this.getItemMeta();
-
-        // the following code changes the appearence of the berry
-        meta.setDisplayName(ChatColor.LIGHT_PURPLE + "Pinkberry");
-
-        ArrayList<String> lore = new ArrayList<>();
-//        lore.add(ChatColor.GRAY + "");
-//        lore.add(ChatColor.GOLD + "Exchange " + ChatColor.YELLOW + "" + ChatColor.BOLD + "RIGHT-CLICK");
-//        lore.add(ChatColor.GRAY + "Converts into 10 " + "raspberries");
-//        lore.add(ChatColor.GRAY + "");
-//        lore.add(ChatColor.GOLD + "Exchange " + ChatColor.YELLOW + "" + ChatColor.BOLD + "LEFT-CLICK");
-//        lore.add(ChatColor.GRAY + "Converts 10 into a rainbowberry");
-        lore.add(ChatColor.GRAY + "");
-        lore.add(ChatColor.DARK_GRAY + "Berries are the currency of Berrycraft.");
-        lore.add(ChatColor.DARK_GRAY + "They can be used to trade in the auction");
-        lore.add(ChatColor.DARK_GRAY + "house or enchant items.");
-
-        lore.add(ChatColor.GRAY + "");
-
-        lore.add(ChatColor.YELLOW + "Currency Item");
-        meta.setLore(lore);
+        meta.setCustomModelData(1);
+        meta.setDisplayName(ChatColor.YELLOW + "Silver Coin");
 
         this.setItemMeta(meta);
         NBTItem nbti = new NBTItem(this);
-
-        // the nbti CustomItem key is used to denote what items are pinkberries
         nbti.setString("CustomItem","Pinkberry");
         nbti.applyNBT(this);
         this.setAmount(1);
@@ -57,28 +36,11 @@ public class Pinkberry extends CustomItem{
     // use new Pinkberry(amount) to get an item stack containing said amount of pinkberry
     public Pinkberry(int amount) {
 
-        super(Material.PLAYER_HEAD);
+        super(Material.IRON_INGOT);
 
-        setSkull("http://textures.minecraft.net/texture/778308fe4dda1bb8a96f5b226b32542f49fd65bc55b44bbc31343eb400cf5e2");
         ItemMeta meta = this.getItemMeta();
-
-        meta.setDisplayName(ChatColor.LIGHT_PURPLE + "Pinkberry");
-        ArrayList<String> lore = new ArrayList<>();
-//        lore.add(ChatColor.GRAY + "");
-//        lore.add(ChatColor.GOLD + "Exchange " + ChatColor.YELLOW + "" + ChatColor.BOLD + "RIGHT-CLICK");
-//        lore.add(ChatColor.GRAY + "Converts into 10 " + "raspberries");
-//        lore.add(ChatColor.GRAY + "");
-//        lore.add(ChatColor.GOLD + "Exchange " + ChatColor.YELLOW + "" + ChatColor.BOLD + "LEFT-CLICK");
-//        lore.add(ChatColor.GRAY + "Converts 10 into a rainbowberry");
-        lore.add(ChatColor.GRAY + "");
-        lore.add(ChatColor.DARK_GRAY + "Berries are the currency of Berrycraft.");
-        lore.add(ChatColor.DARK_GRAY + "They can be used to trade in the auction");
-        lore.add(ChatColor.DARK_GRAY + "house or enchant items.");
-
-        lore.add(ChatColor.GRAY + "");
-
-        lore.add(ChatColor.YELLOW + "Currency Item");
-        meta.setLore(lore);
+        meta.setCustomModelData(1);
+        meta.setDisplayName(ChatColor.YELLOW + "Silver Coin");
 
         this.setItemMeta(meta);
         NBTItem nbti = new NBTItem(this);
@@ -88,14 +50,14 @@ public class Pinkberry extends CustomItem{
 
     }
     public static int getAmount(ItemStack stack) {
-        if (stack.getType()!=Material.PLAYER_HEAD) return 0;
+        if (stack.getType()!=Material.IRON_INGOT) return 0;
         NBTItem nbti = new NBTItem(stack);
         return nbti.getString("CustomItem").equals("Pinkberry") ? stack.getAmount() : 0;
     }
     public static int getAmount(Player p) {
         int total = 0;
         for (ItemStack stack : p.getInventory().getContents()) {
-            if (stack== null || stack.getType()!=Material.PLAYER_HEAD) continue;
+            if (stack== null || stack.getType()!=Material.IRON_INGOT) continue;
             else {
                 NBTItem nbti = new NBTItem(stack);
                 total += nbti.getString("CustomItem").equals("Pinkberry") ? stack.getAmount() : 0;
--- berry-economy/src/main/java/me/berrycraft/berryeconomy/items/Rainbowberry.java
+++ berry-economy/src/main/java/me/berrycraft/berryeconomy/items/Rainbowberry.java
@@ -21,26 +21,11 @@ public class Rainbowberry extends CustomItem {
 
     public Rainbowberry() {
 
-        super(Material.PLAYER_HEAD);
+        super(Material.GOLD_INGOT);
 
-        setSkull("http://textures.minecraft.net/texture/496f07d831d07fe1dd698569a974d2fcd8d83f7583b3877e89c9968b007f3a5d");
         ItemMeta meta = this.getItemMeta();
-
-        meta.setDisplayName(ChatColor.RED + "R" + ChatColor.GOLD+"a"+ChatColor.YELLOW+"i"+ChatColor.GREEN+"n"+ChatColor.BLUE+"b"+ChatColor.DARK_PURPLE+"o"+ChatColor.LIGHT_PURPLE+"w"+ChatColor.RED + "b" + ChatColor.GOLD+"e"+ChatColor.YELLOW+"r"+ChatColor.GREEN+"r"+ChatColor.BLUE+"y");
-
-        ArrayList<String> lore = new ArrayList<>();
-//        lore.add(ChatColor.GRAY + "");
-//        lore.add(ChatColor.GOLD + "Exchange " + ChatColor.YELLOW + "" + ChatColor.BOLD + "RIGHT-CLICK");
-//        lore.add(ChatColor.GRAY + "Can be converted into 10 " + "pinkberries");
-        lore.add(ChatColor.GRAY + "");
-        lore.add(ChatColor.DARK_GRAY + "Berries are the currency of Berrycraft.");
-        lore.add(ChatColor.DARK_GRAY + "They can be used to trade in the auction");
-        lore.add(ChatColor.DARK_GRAY + "house or enchant items.");
-
-        lore.add(ChatColor.GRAY + "");
-
-        lore.add(ChatColor.YELLOW + "Currency Item");
-        meta.setLore(lore);
+        meta.setCustomModelData(1);
+        meta.setDisplayName(ChatColor.YELLOW + "Gold Coin");
         this.setItemMeta(meta);
         NBTItem nbti = new NBTItem(this);
         nbti.setString("CustomItem","Rainbowberry");
@@ -53,29 +38,12 @@ public class Rainbowberry extends CustomItem {
 
     public Rainbowberry(int amount) {
 
-        super(Material.PLAYER_HEAD);
+        super(Material.GOLD_INGOT);
 
-        setSkull("http://textures.minecraft.net/texture/496f07d831d07fe1dd698569a974d2fcd8d83f7583b3877e89c9968b007f3a5d");
         ItemMeta meta = this.getItemMeta();
-
-        meta.setDisplayName(ChatColor.RED + "R" + ChatColor.GOLD+"a"+ChatColor.YELLOW+"i"+ChatColor.GREEN+"n"+ChatColor.BLUE+"b"+ChatColor.DARK_PURPLE+"o"+ChatColor.LIGHT_PURPLE+"w"+ChatColor.RED + "b" + ChatColor.GOLD+"e"+ChatColor.YELLOW+"r"+ChatColor.GREEN+"r"+ChatColor.BLUE+"y");
-
-        ArrayList<String> lore = new ArrayList<>();
-//        lore.add(ChatColor.GRAY + "");
-//        lore.add(ChatColor.GOLD + "Exchange " + ChatColor.YELLOW + "" + ChatColor.BOLD + "RIGHT-CLICK");
-//        lore.add(ChatColor.GRAY + "Can be converted into 10 " + "pinkberries");
-        lore.add(ChatColor.GRAY + "");
-        lore.add(ChatColor.DARK_GRAY + "Berries are the currency of Berrycraft.");
-        lore.add(ChatColor.DARK_GRAY + "They can be used to trade in the auction");
-        lore.add(ChatColor.DARK_GRAY + "house or enchant items.");
-
-        lore.add(ChatColor.GRAY + "");
-
-        lore.add(ChatColor.YELLOW + "Currency Item");
-        meta.setLore(lore);
+        meta.setCustomModelData(1);
+        meta.setDisplayName(ChatColor.YELLOW + "Gold Coin");
         this.setItemMeta(meta);
-
-        // the nbti CustomItem key is used to denote what items are rainbowberries
         NBTItem nbti = new NBTItem(this);
         nbti.setString("CustomItem","Rainbowberry");
         nbti.applyNBT(this);
@@ -83,14 +51,14 @@ public class Rainbowberry extends CustomItem {
 
     }
     public static int getAmount(ItemStack stack) {
-        if (stack.getType()!=Material.PLAYER_HEAD) return 0;
+        if (stack.getType()!=Material.GOLD_INGOT) return 0;
         NBTItem nbti = new NBTItem(stack);
         return nbti.getString("CustomItem").equals("Rainbowberry") ? stack.getAmount() : 0;
     }
     public static int getAmount(Player p) {
         int total = 0;
         for (ItemStack stack : p.getInventory().getContents()) {
-            if (stack== null || stack.getType()!=Material.PLAYER_HEAD) continue;
+            if (stack== null || stack.getType()!=Material.GOLD_INGOT) continue;
             else {
                 NBTItem nbti = new NBTItem(stack);
                 total += nbti.getString("CustomItem").equals("Rainbowberry") ? stack.getAmount() : 0;
